80473b762c52b951c337f48b6c5b0540174f6c2f
Nishan Wickramarathna <nishan.wickramarathna@xeynergy.com>
2024-09-07 03:05:39 +0530

    Add DbContext and Controller to services

--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
+using DotnetIdentityDemo.Data;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Register SQLite database
+builder.Services.AddDbContext<DatabaseContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("TodoContext")));
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -29,5 +38,5 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.MapControllers();
 app.Run();
